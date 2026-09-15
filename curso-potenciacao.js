67d3f1cfaa29430460e636c7947bbba24b272951
PamelaSay <pamelasayao@gmail.com>
2026-09-15 17:22:57 -0300

    decima quatorze dois

--- curso-potenciacao.js
+++ curso-potenciacao.js
@@ -337,7 +337,7 @@ function abrirVideoaula(numero, videoId) {
     titulo.textContent =
         "Aula " +
         numero +
-        " — Multiplicação";
+        " — Potenciação;
 
     if (aviso) {
         const aulaJaConcluida =
